d3322735558b8c8e7e9ae3189d4f96eeafd667d1
Seo <kirin2211@naver.com>
2023-02-02 18:35:40 +0900

    button event

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Button from "./Button";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => {
+    setCount((prev) => prev + 1);
+    console.log(count);
+  };
+
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text="hello" />
+      <h1>{count}</h1>
+      <button onClick={onClick}>click me!</button>
     </div>
   );
 }
