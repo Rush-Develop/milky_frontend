5e8c55c0cb34849b8508f958979d4e0b605621ce
Seo <kirin2211@naver.com>
2023-02-03 17:27:31 +0900

    error

--- src/App.js
+++ src/App.js
@@ -6,31 +6,21 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movie, setMovie] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    )
+      .then((response) => {
+        response.json();
+      })
       .then((json) => {
-        setCoins(json);
+        setMovie(json.data.movies);
         setLoading(false);
+        console.log(movie);
       });
   }, []);
-  return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name}({coin.symbol}) : {coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return <div>{loading ? <h1>loading...</h1> : null}</div>;
 }
 
 export default App;
