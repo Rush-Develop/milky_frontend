0692a2b65d10e8d32868144eaedb92d6dc8db6a5
Seo <kirin2211@naver.com>
2023-02-04 16:00:01 +0900

    movie select

--- src/App.js
+++ src/App.js
@@ -7,20 +7,31 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovie(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    )
-      .then((response) => {
-        response.json();
-      })
-      .then((json) => {
-        setMovie(json.data.movies);
-        setLoading(false);
-        console.log(movie);
-      });
+    getMovies();
   }, []);
-  return <div>{loading ? <h1>loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          {movie.map((movie) => (
+            <div>{movie.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
