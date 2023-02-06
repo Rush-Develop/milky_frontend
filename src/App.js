bd03a430bfcb51493630bb7aee1a6e02200edd9c
Seo <kirin2211@naver.com>
2023-02-06 16:21:22 +0900

    router' git push

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import Button from "./Button";
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movie, setMovie] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-    setMovie(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>loading...</h1>
-      ) : (
-        <div>
-          {movie.map((movie) => (
-            <div>{movie.title}</div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
