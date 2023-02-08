2a748d9a36150c94dd9656967789cae642d5f547
Seo <kirin2211@naver.com>
2023-02-08 21:57:25 +0900

    sidebar scroll event

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,16 +5,29 @@ import styles from "./components/home.module.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Section from "./components/Section";
+import Menu from "./components/Menu";
 
 function Home() {
   return (
     <div>
-      <Sidebar width={360} />
+      <Sidebar width={360}>
+        <Menu />
+      </Sidebar>
       <div className={styles.grid}>
         <Header />
         <div className={styles.Main}>
-          <Section cnt="1" />
-          <Section cnt="2" />
+          <div id="1">
+            <Section cnt="1" />
+          </div>
+          <div id="2">
+            <Section cnt="2" />
+          </div>
+          <div id="3">
+            <Section cnt="3" />
+          </div>
+          <div id="4">
+            <Section cnt="4" />
+          </div>
         </div>
         <div className={styles.Footer}>Footer</div>
       </div>
