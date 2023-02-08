06e1d021d3bd4144c860fa4b0509ba06bfff1a6e
Seo <kirin2211@naver.com>
2023-02-08 16:38:47 +0900

    section separate

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,6 +3,8 @@ import Movie from "./Movie";
 import Sidebar from "./components/Sidebar";
 import styles from "./components/home.module.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Section from "./components/Section";
 
 function Home() {
   return (
@@ -10,7 +12,10 @@ function Home() {
       <Sidebar width={360} />
       <div className={styles.grid}>
         <Header />
-        <div className={styles.Main}>Main</div>
+        <div className={styles.Main}>
+          <Section cnt="1" />
+          <Section cnt="2" />
+        </div>
         <div className={styles.Footer}>Footer</div>
       </div>
     </div>
--- src/routes/components/Header.js
+++ src/routes/components/Header.js
@@ -3,12 +3,13 @@ import styles from "../components/home.module.css";
 function Header() {
   return (
     <div className={styles.Header}>
-      <img
+      {/* <img
         className={styles.Logo}
         src="assets/img/logo.jpeg"
         alt="logo"
         style={{ width: 100, height: 75, margin: 0, padding: 0 }}
-      />
+      /> */}
+      Logo
     </div>
   );
 }
