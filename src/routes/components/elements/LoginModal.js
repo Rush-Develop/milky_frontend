21585e98af07e81d3f8bd78e041c9ef50992d98a
Seo <kirin2211@naver.com>
2023-02-11 15:31:17 +0900

    login form

--- src/routes/components/elements/LoginModal.js
+++ src/routes/components/elements/LoginModal.js
@@ -1,9 +1,21 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../css/home.module.css";
 import Modal from "react-modal";
 
 function LoginModal() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+  console.log("렌더링");
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
   return (
     <>
       <button onClick={() => setModalIsOpen(true)} className={styles.Login}>
@@ -25,9 +37,9 @@ function LoginModal() {
           content: {
             position: "absolute",
             top: "200px",
-            left: "200px",
-            right: "200px",
-            bottom: "200px",
+            left: "400px",
+            right: "400px",
+            bottom: "300px",
             border: "1px solid #ccc",
             background: "#fff",
             overflow: "auto",
@@ -42,7 +54,31 @@ function LoginModal() {
         <button onClick={() => setModalIsOpen(false)} className={styles.Login}>
           로그인
         </button>
-        <p className={styles.Modal}>hello</p>
+        <p className={styles.Modal}>
+          <form style={{ textAlign: "center", paddingTop: "50px" }}>
+            <p className={styles.UI}>
+              <input
+                type="text"
+                id="email"
+                placeholder="Enter email"
+                value={state.email}
+                onChange={handleChange}
+              />
+            </p>
+            <p className={styles.UI}>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={state.password}
+                onChange={handleChange}
+              />
+            </p>
+            <p className={styles.UI}>
+              <input type="submit" value="Login" />
+            </p>
+          </form>
+        </p>
       </Modal>
     </>
   );
--- src/routes/components/elements/Menu.js
+++ src/routes/components/elements/Menu.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
+import LoginModal from "./LoginModal";
 
 function Menu() {
   const StyledLink = styled(Link)`
