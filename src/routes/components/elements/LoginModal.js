8267f81ce59d0001ebf477e933a9f9faf5f6adec
Seo <kirin2211@naver.com>
2023-02-13 20:27:42 +0900

    Login position fixed

--- src/routes/components/elements/LoginModal.js
+++ src/routes/components/elements/LoginModal.js
@@ -22,21 +22,30 @@ export default function FormDialog() {
   return (
     <div className={styles.modal}>
       <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
+        Login
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>Login</DialogTitle>
         <DialogContent>
-          <DialogContentText>
+          {/* <DialogContentText>
             To subscribe to this website, please enter your email address here.
             We will send updates occasionally.
-          </DialogContentText>
+          </DialogContentText> */}
           <TextField
             autoFocus
             margin="dense"
             id="name"
             label="Email Address"
             type="email"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Password"
+            type="password"
             fullWidth
             variant="standard"
           />
