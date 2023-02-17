88609c59b8e576d5b1a4d59189e8aea3cf06143f
Seo <kirin2211@naver.com>
2023-02-17 18:24:08 +0900

    react and express server connect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("connect!");
+  res.send("Connect!");
+});
+
+app.post("/idplz", (req, res) => {
+  const serverid = req.body.plzid;
+  console.log(serverid);
 });
 
 app.listen(port, () => {
-  console.log(`Connect at http://localhost:${port}`); // '가 아닌 좌측상단의 esc버튼 밑의 `다.
+  console.log(`Connect at http://localhost:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 function App() {
+  const [id, setId] = useState("");
+  const handleChange = (event) => {
+    setId(event.target.value);
+  };
+  const submitId = () => {
+    const post = {
+      plzid: id,
+    };
+
+    fetch("http://localhost:3001/idplz", {
+      method: "post", // 통신방법
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(post),
+    });
+  };
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      {/* <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router> */}
+      <input onChange={handleChange} name="id" />
+      <button onClick={submitId}>Submit</button>
+      <h1>{id}</h1>
+    </div>
   );
 }
 
