e3f5bc8d730fd29847868181c09a6b6fbd062574
Seo <kirin2211@naver.com>
2023-02-18 19:11:20 +0900

    discord api get

--- server/server.js
+++ server/server.js
@@ -1,22 +1,19 @@
+const path = require("path");
 const express = require("express");
+
 const app = express();
-const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
-const cors = require("cors");
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use("", express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.send("Connect!");
+app.get("/", (request, response) => {
+  return response.sendFile("main.html", { root: "." });
 });
 
-app.post("/idplz", (req, res) => {
-  const serverid = req.body.plzid;
-  console.log(serverid);
+app.get("/auth/discord", (request, response) => {
+  return response.sendFile("dashboard.html", { root: "." });
 });
 
-app.listen(port, () => {
-  console.log(`Connect at http://localhost:${port}`);
-});
+const port = "3001";
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
