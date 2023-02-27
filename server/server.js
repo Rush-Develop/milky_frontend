2d37adce5e8335f7a5093c0bcdfab7163ccf5967
Seo <kirin2211@naver.com>
2023-02-27 15:18:11 +0900

    hello

--- server/server.js
+++ server/server.js
@@ -9,9 +9,8 @@ app.get("/", (request, response) => {
   return response.sendFile("main.html", { root: "." });
 });
 
-app.post("/idplz", (req, res) => {
-  const serverid = req.body.plzid;
-  console.log(serverid);
+app.get("/auth/discord", (request, response) => {
+  return response.redirect("http://localhost:3000");
 });
 
 const port = "8080";
