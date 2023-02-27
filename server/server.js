90f6f7d5c17de197caa70ec3cbb155956141a2b3
Seo <kirin2211@naver.com>
2023-02-27 19:47:25 +0900

    code reformat

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.get("/", (request, response) => {
   return response.sendFile("main.html", { root: "." });
 });
 
-app.get("/join", (request, response) => {
+app.get("/login/oauth2/code/discord", (request, response) => {
   console.log(response);
   //이 사이에 이제 DB랑 BACK에 넘겨준 response랑 비교해서 회원이 되있으면 redirect
   return response.redirect("http://localhost:3000");
--- src/routes/components/elements/LoginModal.js
+++ src/routes/components/elements/LoginModal.js
@@ -13,9 +13,46 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Zoom timeout={1000} ref={ref} {...props} />;
 });
 
-function LoginModal(props) {
-  const [open, setOpen] = useState(false);
+function LoginModal() {
+  const [open, setOpen] = useState(true);
+  const [Id, setId] = useState("");
   const [username, setUsername] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  // componentDidMount = async () => {
+  //   const response = await fetch("http://date.jsontest.com");
+  //   const body = await response.json();
+  //   alert(body.date);
+  // };
+
+  const fragment = new URLSearchParams(window.location.hash.slice(1));
+  console.log(window.location.hash.slice(1));
+  const [accessToken, tokenType] = [
+    fragment.get("access_token"),
+    fragment.get("token_type"),
+  ];
+  console.log(accessToken);
+  console.log(tokenType);
+
+  // if (!accessToken) {
+  //   window.location.href = "/";
+  // }
+  useEffect(() => {
+    fetch("https://discord.com/api/users/@me", {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`,
+        // authorization: `nice hello`,
+      },
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        console.log(response);
+        setId(response.id);
+        setUsername(response.username);
+        setAvatar(response.avatar);
+      })
+      .catch(console.error);
+  }, []);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -30,19 +67,12 @@ function LoginModal(props) {
     },
   };
 
-  // if (props.id === undefined) {
-  // } else {
-  //   console.log(props.id);
-  //   console.log(props.username);
-  //   console.log(props.avatar);
-  // }
-
   return (
     <div className={styles.headprofile}>
-      {props.id === undefined ? (
+      {Id === undefined ? (
         <a
           id="login"
-          href="https://discord.com/api/oauth2/authorize?client_id=1060823658832076830&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fjoin&response_type=token&scope=identify"
+          href="https://discord.com/api/oauth2/authorize?client_id=1060823658832076830&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Flogin%2Foauth2%2Fcode%2Fdiscord&response_type=token&scope=identify%20email"
           style={{ textDecoration: "none" }}
         >
           <Button variant="text" sx={styled}>
@@ -58,7 +88,7 @@ function LoginModal(props) {
             onClick={handleClickOpen}
           >
             <img
-              src={`https://cdn.discordapp.com/avatars/${props.id}/${props.avatar}.jpg`}
+              src={`https://cdn.discordapp.com/avatars/${Id}/${avatar}.jpg`}
               alt="my"
               width={"70px"}
             />
@@ -74,27 +104,27 @@ function LoginModal(props) {
               <header className={styles.header}>
                 <img
                   className={styles.profileimg}
-                  src={`https://cdn.discordapp.com/avatars/${props.id}/${props.avatar}.jpg`}
+                  src={`https://cdn.discordapp.com/avatars/${Id}/${avatar}.jpg`}
                   alt="my"
                   width={"70px"}
                 />
               </header>
               <h1 className={styles.boldtext}>
-                {props.username}
-                <span className={styles.normaltext}>{props.age}</span>
+                {username}
+                {/* <span className={styles.normaltext}>age</span> */}
               </h1>
-              <h2 className={styles.normaltext}>{props.city}</h2>
+              <h2 className={styles.normaltext}>정보0</h2>
               <div className={styles.socialcontainer}>
                 <div className={styles.followers}>
-                  <h1 className={styles.boldtext}>{props.followers}</h1>
+                  <h1 className={styles.boldtext}>정보1</h1>
                   <h2 className={styles.smallertext}>정보1</h2>
                 </div>
                 <div className={styles.likes}>
-                  <h1 className={styles.boldtext}>{props.likes}</h1>
+                  <h1 className={styles.boldtext}>정보2</h1>
                   <h2 className={styles.smallertext}>정보2</h2>
                 </div>
                 <div className={styles.photos}>
-                  <h1 className={styles.boldtext}>{props.photos}</h1>
+                  <h1 className={styles.boldtext}>정보3</h1>
                   <h2 className={styles.smallertext}>정보3</h2>
                 </div>
               </div>
@@ -106,7 +136,7 @@ function LoginModal(props) {
             </DialogActions>
           </Dialog>
         </span>
-        // <span className={styles.profile}>{props.username}님 안녕하세요!</span>
+        // {/* <span className={styles.profile}>{props.username}님 안녕하세요!</span> */}
       )}
     </div>
   );
